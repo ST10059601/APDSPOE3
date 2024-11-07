15de2c993d8c3f935fd4917088ebde271a587841
ST10059601 <104444715+ST10059601@users.noreply.github.com>
2024-11-07 14:13:14 +0200

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -10,7 +10,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch('http://localhost:3000/user/login', {  
+    const response = await fetch('https://localhost:3000/user/login', {  
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
